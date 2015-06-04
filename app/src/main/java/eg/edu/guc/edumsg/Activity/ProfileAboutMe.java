1b03874ed5d551dafead01fafdc57b21e184d40b
ammarwa <ammarwa@gmail.com>
2015-06-04 21:47:48 +0300

    Adding About Me

--- app/src/main/java/eg/edu/guc/edumsg/Activity/ProfileAboutMe.java
+++ app/src/main/java/eg/edu/guc/edumsg/Activity/ProfileAboutMe.java
@@ -6,7 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import eg.edu.guc.edumsg.Model.User;
 import eg.edu.guc.edumsg.R;
 
 /**
@@ -24,7 +26,28 @@ public class ProfileAboutMe extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_about_me, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile_about_me, container, false);
+        TextView name = (TextView) v.findViewById(R.id.prof_name);
+        TextView email = (TextView) v.findViewById(R.id.email_profile_info);
+        TextView gender = (TextView) v.findViewById(R.id.gender_profile_info);
+        TextView country = (TextView) v.findViewById(R.id.country_profile_info);
+        TextView dateOfBirth = (TextView) v.findViewById(R.id.date_of_birth_profile_info);
+        TextView language = (TextView) v.findViewById(R.id.language_profile_info);
+        TextView website = (TextView) v.findViewById(R.id.website_profile_info);
+        TextView bio = (TextView) v.findViewById(R.id.bio_profile_info);
+
+        User currentUser = ((MainActivity)getActivity()).getLoggedIn();
+
+        name.setText(currentUser.getFirst_name()+" "+currentUser.getLast_name());
+        email.setText(currentUser.getEmail());
+        gender.setText(currentUser.getGender());
+        country.setText(currentUser.getCountry());
+        dateOfBirth.setText(currentUser.getDate_of_birth());
+        language.setText(currentUser.getLanguage());
+        website.setText(currentUser.getWebsite());
+        bio.setText(currentUser.getBio());
+
+        return v;
     }
 
 
